8b68889bdf859af2c2e0b9ae579933dd6ceab1f3
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-11-11 14:08:26 +0000

    chore: total cartoons features added

--- manufacturing_addon/manufacturing_addon/doctype/order_sheet/order_sheet.py
+++ manufacturing_addon/manufacturing_addon/doctype/order_sheet/order_sheet.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2024, mohtashim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class OrderSheet(Document):
-	pass
+	def validate(self):
+		self.total_cartoon()
+	
+	def total_cartoon(self):
+		total_cartoon = 0
+		for row in self.order_sheet_ct:
+			qty_ctn = row.qty_ctn
+			quantity = row.quantity
+			total_cartoons = qty_ctn / quantity
+			row.total_cartoons = total_cartoons

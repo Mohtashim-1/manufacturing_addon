0984874e618986ee824d3171cee281edcf3cc0d7
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-04-21 19:04:43 +0500

    chore: fix

--- manufacturing_addon/api.py
+++ manufacturing_addon/api.py
@@ -50,32 +50,9 @@ def create_order_sheet(sales_order):
         return {"error": str(e)}
 
 
-@frappe.whitelist()
 def add_parameter(doc, method):
-    pass
-#     """Fetches and updates custom item parameters from Item Category."""
-    
-#     if doc.custom_item_category:
-#         # frappe.msgprint(("Fetching parameters from Item Category..."))
-
-#         # Fetch the Item Category document
-#         category = frappe.get_doc("Item Category", doc.custom_item_category)
-
-#         if category.custom_item_parameter:
-#             # Clear existing rows
-#             doc.set("custom_item_parameter", [])
-
-#             # Add new rows from category
-#             for param in category.custom_item_parameter:
-#                 row = doc.append("custom_item_parameter", {})
-#                 row.parameter = param.parameter
-
-#                 # Check if 'value' exists before assigning
-#                 # if hasattr(param, "value"):  
-#                 #     row.value = param.value
-
-#             # frappe.msgprint(("Custom Item Parameters updated from Item Category."))
-#         else:
-#             frappe.msgprint(("No parameters found in the selected Item Category."))
-#     else:
-#         frappe.msgprint(("No Item Category selected."))
+    if doc.custom_item_category and not doc.custom_item_parameter:
+        category = frappe.get_doc("Item Category", doc.custom_item_category)
+        for param in category.custom_item_parameter:
+            row = doc.append("custom_item_parameter", {})
+            row.parameter = param.parameter

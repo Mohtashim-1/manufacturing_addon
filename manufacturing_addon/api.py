c78f2028fd198fb739be462079ed5186bb932e4d
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-04-21 16:09:23 +0500

    chore: fix in item paramter

--- manufacturing_addon/api.py
+++ manufacturing_addon/api.py
@@ -52,29 +52,30 @@ def create_order_sheet(sales_order):
 
 @frappe.whitelist()
 def add_parameter(doc, method):
-    """Fetches and updates custom item parameters from Item Category."""
+    pass
+#     """Fetches and updates custom item parameters from Item Category."""
     
-    if doc.custom_item_category:
-        # frappe.msgprint(("Fetching parameters from Item Category..."))
+#     if doc.custom_item_category:
+#         # frappe.msgprint(("Fetching parameters from Item Category..."))
 
-        # Fetch the Item Category document
-        category = frappe.get_doc("Item Category", doc.custom_item_category)
+#         # Fetch the Item Category document
+#         category = frappe.get_doc("Item Category", doc.custom_item_category)
 
-        if category.custom_item_parameter:
-            # Clear existing rows
-            doc.set("custom_item_parameter", [])
+#         if category.custom_item_parameter:
+#             # Clear existing rows
+#             doc.set("custom_item_parameter", [])
 
-            # Add new rows from category
-            for param in category.custom_item_parameter:
-                row = doc.append("custom_item_parameter", {})
-                row.parameter = param.parameter
+#             # Add new rows from category
+#             for param in category.custom_item_parameter:
+#                 row = doc.append("custom_item_parameter", {})
+#                 row.parameter = param.parameter
 
-                # Check if 'value' exists before assigning
-                # if hasattr(param, "value"):  
-                #     row.value = param.value
+#                 # Check if 'value' exists before assigning
+#                 # if hasattr(param, "value"):  
+#                 #     row.value = param.value
 
-            # frappe.msgprint(("Custom Item Parameters updated from Item Category."))
-        else:
-            frappe.msgprint(("No parameters found in the selected Item Category."))
-    else:
-        frappe.msgprint(("No Item Category selected."))
+#             # frappe.msgprint(("Custom Item Parameters updated from Item Category."))
+#         else:
+#             frappe.msgprint(("No parameters found in the selected Item Category."))
+#     else:
+#         frappe.msgprint(("No Item Category selected."))

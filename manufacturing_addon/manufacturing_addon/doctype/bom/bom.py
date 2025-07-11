e4c8ca6dcb2e2a55dc3a9bf6ac8046643c80a65b
Mohtashim-1 <shoaibmohtashin973@gmail.com>
2025-07-11 12:47:02 +0500

    chore: add bom template

--- manufacturing_addon/manufacturing_addon/doctype/bom/bom.py
+++ manufacturing_addon/manufacturing_addon/doctype/bom/bom.py
@@ -9,3 +9,4 @@ def duplicate_item(doc, method):
                 _(f"Duplicate not allowed: <b>{item.item_code}</b> is already present in the BOM at row <b>{item_codes[item.item_code]}</b> and again at row <b>{item.idx}</b>. Please ensure each item is only listed once in the BOM. Remove the duplicate entry to proceed.")
             )
         item_codes[item.item_code] = item.idx
+        
